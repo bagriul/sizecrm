79754dabb5306de4548397b26037e565eb0f6da7
bagriul <bagriul@gmail.com>
2023-10-20 16:47:50 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'sizecrmsecretkey'
+MONGO_STRING = 'mongodb+srv://tsbgalcontract:mymongodb26@cluster0.kppkt.mongodb.net/test?authSource=admin&replicaSet=atlas-8jvx35-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
