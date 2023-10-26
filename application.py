755270c77284eaba71205d93ce57270e50f2899f
bagriul <bagriul@gmail.com>
2023-10-26 14:52:39 +0300

    Added update client endpoint

--- application.py
+++ application.py
@@ -182,14 +182,33 @@ def add_client():
 # Endpoint to get clients list
 @application.route('/clients', methods=['GET'])
 def clients():
-    # Retrieve all clients from the MongoDB collection
-    clients = list(clients_collection.find())
+    data = request.get_json()
+    keyword = data.get('keyword')
+    page = data.get('page', 1)  # Default to page 1 if not provided
+    per_page = data.get('per_page', 10)  # Default to 10 items per page if not provided
 
-    # Convert BSON documents to JSON format
-    clients_json = json_util.dumps(clients)
+    filter_criteria = {}
+    if keyword:
+        clients_collection.create_index([("$**", "text")])
+        filter_criteria['$text'] = {'$search': keyword}
 
-    # Return the clients as a JSON response
-    return clients_json, 200
+    # Count the total number of clients that match the filter criteria
+    total_clients = clients_collection.count_documents(filter_criteria)
+
+    # Paginate the query results using skip and limit, and apply filters
+    skip = (page - 1) * per_page
+    documents = list(clients_collection.find(filter_criteria).skip(skip).limit(per_page))
+
+    # Calculate the range of clients being displayed
+    start_range = skip + 1
+    end_range = min(skip + per_page, total_clients)
+
+    # Serialize the documents using json_util from pymongo and specify encoding
+    response = Response(json_util.dumps(
+        {'clients': documents, 'total_clients': total_clients, 'start_range': start_range, 'end_range': end_range},
+        ensure_ascii=False).encode('utf-8'),
+                        content_type='application/json;charset=utf-8')
+    return response, 200
 
 
 # Endpoint to delete client
@@ -208,6 +227,80 @@ def delete_client():
         return jsonify({'message': 'Client deleted successfully'}), 200
     else:
         return jsonify({'message': 'Client not found'}), 404
+
+
+# Endpoint to edit client
+@application.route('/update_client/<client_id>', methods=['PUT'])
+def update_client(client_id):
+    try:
+        # Convert the client_id to ObjectId type
+        client_object_id = ObjectId(client_id)
+
+        # Retrieve the existing client document from MongoDB
+        existing_client = clients_collection.find_one({'_id': client_object_id})
+
+        if existing_client:
+            # Get data from the request
+            userpic = request.files.get('userpic')
+            data = request.form.to_dict()
+
+            # Update fields if new data is provided in the request
+            if 'name' in data:
+                existing_client['name'] = data['name']
+            if 'phone' in data:
+                existing_client['phone'] = data['phone']
+            if 'additional_phone' in data:
+                existing_client['additional_phone'] = data['additional_phone']
+            if 'email' in data:
+                existing_client['email'] = data['email']
+            if 'gender' in data:
+                existing_client['gender'] = data['gender']
+            if 'birthday' in data:
+                existing_client['birthday'] = data['birthday']
+            if 'instagram' in data:
+                existing_client['instagram'] = data['instagram']
+            if 'telegram' in data:
+                existing_client['telegram'] = data['telegram']
+            if 'comment' in data:
+                existing_client['comment'] = data['comment']
+            if 'status' in data:
+                existing_client['status'] = data['status']
+
+            # Update userpic if a new userpic is provided in the request
+            def process_and_store_userpic(userpic):
+                if userpic:
+                    try:
+                        # Read the image file as binary data
+                        image_data = userpic.read()
+
+                        # Encode the binary image data as base64
+                        base64_image = base64.b64encode(image_data).decode('utf-8')
+
+                        # Return the base64 encoded image data for MongoDB storage
+                        return base64_image
+
+                    except Exception as e:
+                        # Handle any potential errors while processing the image
+                        print(f"Error processing image: {str(e)}")
+                        return None
+
+                else:
+                    # Handle invalid or missing userpic files
+                    return None
+            if userpic:
+                existing_client['userpic'] = process_and_store_userpic(userpic)
+
+            # Update the client document in MongoDB
+            clients_collection.find_one_and_update({'_id': client_object_id}, {'$set': existing_client})
+
+            return jsonify({'message': 'Client updated successfully'}), 200
+
+        else:
+            return jsonify({'message': 'Client not found'}), 404
+
+    except Exception as e:
+        # Handle errors
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
